234a5c34d658985e2d7aebcb1ee0ab331f0e879e
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-05-23 23:50:44 -0500

    Cleaned up altPS.js

--- altParticleSystem.js
+++ altParticleSystem.js
@@ -1,31 +1,31 @@
 // New Triangle(x, y);
 
 class altParticleSystem {
- constructor(x, y, magnitude, sides, size) {
-   this.center = {x, y};
-   this.verticies = []; // [{x, y}] : represents the invisible outter shape
-   this.particles = []; // [Particles()]
-   this.children = [];  // [ParticleSystem()]
-   this.magnitude = createVector(0, magnitude); // y vector -->
-   this.sides = sides || 3; // Sides of shape
-   this.currPercent = 0
-   this.size = size
- }
+  constructor(x, y, magnitude, sides, size) {
+    this.center = { x, y };
+    this.verticies = []; // [{x, y}] : represents the invisible outter shape
+    this.particles = []; // [Particles()]
+    this.children = [];  // [ParticleSystem()]
+    this.magnitude = createVector(0, magnitude); // y vector -->
+    this.sides = sides || 3; // Sides of shape
+    this.currPercent = 0;
+    this.size = size;
+  }
 
   // Function to figure out the location of the furthest verticies
   init() {
     // Create verticies
-    this.magnitude.rotate(random(0,TWO_PI))
-    let angleBetween = TWO_PI  / this.sides;
-    for(let i=0; i<this.sides; i++) {
-      this.verticies.push({ x:this.magnitude.x, y:this.magnitude.y });
+    this.magnitude.rotate(random(0, TWO_PI));
+    let angleBetween = TWO_PI / this.sides;
+    for (let i = 0; i < this.sides; i++) {
+      this.verticies.push({ x: this.magnitude.x, y: this.magnitude.y });
       this.magnitude.rotate(angleBetween);
     }
     // Create inner particles
-    for(let i=0; i<this.verticies.length; i++) {
+    for (let i = 0; i < this.verticies.length; i++) {
       let newX = this.verticies[i].x * this.currPercent;
       let newY = this.verticies[i].y * this.currPercent;
-      this.particles.push({x: newX, y: newY});
+      this.particles.push({ x: newX, y: newY });
     }
     // Draw at 50%
     this.draw();
@@ -38,10 +38,10 @@ class altParticleSystem {
     this.particles = [];
     this.currPercent = percent;
 
-    for(let i=0; i<this.verticies.length; i++) {
+    for (let i = 0; i < this.verticies.length; i++) {
       let newX = this.verticies[i].x * percent;
       let newY = this.verticies[i].y * percent;
-      this.particles.push({x: newX, y: newY});
+      this.particles.push({ x: newX, y: newY });
     }
 
     // Draw verticies
@@ -55,12 +55,12 @@ class altParticleSystem {
     let currPer = map(this.currPercent, 0, 1, 0, 8);
     strokeWeight(2)
     // let currPer = map(this.currPercent, 0, 1, 0, 255);
-    stroke((frameCount/2) % 255, 180, 200, .4);
+    stroke((frameCount / 2) % 255, 180, 200, .4);
     // noFill();
     fill(backgroundCol);
-    
+
     beginShape();
-    for(let i=0; i<this.particles.length; i++) {
+    for (let i = 0; i < this.particles.length; i++) {
       vertex(this.particles[i].x, this.particles[i].y);
     }
     endShape(CLOSE);
